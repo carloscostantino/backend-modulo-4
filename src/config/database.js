9c52d14b296a6ea0f7521fb34fd84fe9a84342ff
carloscostantino <carloscostantino@gmail.com>
2025-12-30 15:23:12 -0300

    Config deploy Railway

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,19 @@
 import { Sequelize } from "sequelize";// Importamos Sequelize
 
-require("dotenv").config();   // Cargamos variables de entorno
+//require("dotenv").config();   // Cargamos variables de entorno cuando trabajo localmente
 
 // Creamos la conexión a la base de datos
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     dialect: "mysql",
   }
 );
+
 
 // Función para probar conexión
 const connectDB = async () => {
