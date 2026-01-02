ad2d1c8234ca73990e9fbcbca3e82158f6a99202
carloscostantino <carloscostantino@gmail.com>
2026-01-02 17:09:10 -0300

    Se actualizan variables de bd

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
 
-const DB_NAME = process.env.DB_NAME || process.env.MYSQLDATABASE;
-const DB_USER = process.env.DB_USER || process.env.MYSQLUSER;
-const DB_PASSWORD = process.env.DB_PASSWORD || process.env.MYSQLPASSWORD;
-const DB_HOST = process.env.DB_HOST || process.env.MYSQLHOST;
-const DB_PORT = process.env.DB_PORT || process.env.MYSQLPORT;
+const DB_NAME = process.env.MYSQLDATABASE;
+const DB_USER = process.env.MYSQLUSER;
+const DB_PASSWORD = process.env.MYSQLPASSWORD;
+const DB_HOST = process.env.MYSQLHOST;
+const DB_PORT = process.env.MYSQLPORT;
 
 const sequelize = new Sequelize(
   DB_NAME,
